fd9e86ac2791e42a4d925e27f08d6cb327287534
GeneralYouri <youribok@msn.com>
2021-01-01 23:42:05 +0100

    Edit webpack config to add an extra notice at the top of compiled files to link to the repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const glob = require('glob');
 const path = require('path');
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const banner = '@see ' + require('./package.json').homepage;
 
 // TODO: Look into caching to prevent the initial compilation when running `npm run watch`
 // This compilation should not be needed, and may become a big nuisance when the project grows bigger
 module.exports = {
     mode: 'production',
+    watchOptions: {
+        ignored: /node_modules/,
+    },
 
+    /** Properly link a variable number of individual entry files to corresponding output files */
     // Match all .mjs files separately, using useful names for `output.filename`
     entry: glob.sync('./src/*/*/index.mjs').reduce((acc, filename) => {
         // The name will be exactly what `*/*` matches in the above glob
@@ -18,7 +26,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
 
-    // Use Babel to transpile to NodeJS v8.10, which is the version as supported by CSES
+    /** Use Babel to transpile to NodeJS v8.10, which is the version as supported by CSES */
     target: 'node8.10',
     module: {
         rules: [
@@ -35,7 +43,24 @@ module.exports = {
         ],
     },
 
-    watchOptions: {
-        ignored: /node_modules/,
+    /** Add repo link at the top of output files */
+    plugins: [
+        new webpack.BannerPlugin({
+            banner,
+            entryOnly: true,
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: new RegExp(banner, 'i'),
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
     },
 };
