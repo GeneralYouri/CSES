ebae75ccce12cd7b5dd092c087a6f6a156ff3658
GeneralYouri <youribok@msn.com>
2021-01-13 01:27:39 +0100

    Add problemset 1754 solution

---
+++ src/problemset/1754.mjs
@@ -0,0 +1,16 @@
+import { runFromCli } from '~/lib.mjs';
+
+const solution = (lines) => {
+    const output = [];
+    for (const line of lines.slice(1)) {
+        const [a, b] = line.split(' ').map(Number);
+        const valid = (a + b) % 3 === 0 && 2 * a >= b && a <= 2 * b;
+        output.push(valid ? 'YES' : 'NO');
+    }
+    return output;
+};
+export default solution;
+
+if (process.env.NODE_ENV !== 'test') {
+    runFromCli(solution);
+}
---
+++ src/problemset/1754.test.mjs
@@ -0,0 +1,5 @@
+import solution from './1754.mjs';
+
+test('Provided test cases', () => {
+    expect(solution(['3', '2 1', '2 2', '3 3'])).toEqual(['YES', 'NO', 'YES']);
+});
