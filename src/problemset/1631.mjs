0c9c9bf02d6c54e7cbcefd857bbf86426c8d5092
GeneralYouri <youribok@msn.com>
2021-01-13 01:07:25 +0100

    Add problemset 1631 solution

---
+++ src/problemset/1631.mjs
@@ -0,0 +1,20 @@
+import { runFromCli } from '~/lib.mjs';
+
+const solution = (lines) => {
+    const _n = lines[0].split(' ').map(Number);
+    const T = lines[1].split(' ').map(Number);
+    let sum = 0;
+    let max = 0;
+    for (const t of T) {
+        sum += t;
+        if (t > max) {
+            max = t;
+        }
+    }
+    return Math.max(sum, 2 * max);
+};
+export default solution;
+
+if (process.env.NODE_ENV !== 'test') {
+    runFromCli(solution);
+}
---
+++ src/problemset/1631.test.mjs
@@ -0,0 +1,5 @@
+import solution from './1631.mjs';
+
+test('Provided test cases', () => {
+    expect(solution(['3', '2 8 3'])).toBe(16);
+});
