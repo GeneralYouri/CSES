63226b9f0c3512e7e7a180a3a8162f65697b5389
GeneralYouri <youribok@msn.com>
2021-01-16 05:36:09 +0100

    Extract some generic features from problemset 1622 and 1713 solutions into a library file for reuse

--- src/problemset/1622.mjs
+++ src/problemset/1622.mjs
@@ -1,24 +1,5 @@
-import { runFromCli } from '~/lib.mjs';
-
-const permutationsUnique = (permutationOptions) => {
-    if (permutationOptions.length <= 1) {
-        return [permutationOptions];
-    }
-
-    const permutations = [];
-    const smallerPermutations = permutationsUnique(permutationOptions.slice(1));
-    const firstOption = permutationOptions[0];
-
-    for (const smallerPermutation of smallerPermutations) {
-        for (let positionIndex = 0; positionIndex <= smallerPermutation.length; positionIndex += 1) {
-            const prefix = smallerPermutation.slice(0, positionIndex);
-            const suffix = smallerPermutation.slice(positionIndex);
-            permutations.push([...prefix, firstOption, ...suffix]);
-        }
-    }
-
-    return permutations;
-};
+import { runFromCli } from '~/library.mjs';
+import { permutationsUnique } from '~/src/library.mjs';
 
 const solution = (lines) => {
     const permutations = permutationsUnique(lines[0].split('')).map(permutation => permutation.join(''));
